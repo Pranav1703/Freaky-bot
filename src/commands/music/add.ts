9a0322551da0e480df90270e90c14011e27cfe71
Pranav1703 <pranavsai2005@gmail.com>
2024-10-09 01:23:43 +0530

    few changes to add command

--- src/commands/music/add.ts
+++ src/commands/music/add.ts
@@ -21,13 +21,9 @@ export async function execute(interaction:ChatInputCommandInteraction){
     const query = interaction.options.getString("query") as string;
     
     const guildNode = usePlayer(interaction.guild?.id as string); //queue.node = guildNode ??
-    console.log("guild NODE ",guildNode)
+    console.log("\nguild NODE ",guildNode)
 
     try {
-        const track = await player.search(query);
-
-            // console.log(track.tracks[0])
-        // queue?.insertTrack(track.tracks[0])  // test this
         
         if(guildNode === null){
             try {
@@ -36,22 +32,30 @@ export async function execute(interaction:ChatInputCommandInteraction){
                         metadata: interaction.channel
                     }
                 })
-                interaction.reply(`Playing **${track.title}**.`)
-            } catch (error) {
+                await interaction.reply(`**${track.title}** added to queue.`)
+                await interaction.followUp(`Playing **${track.title}**.`)
                 
+            } catch (error) {
+                console.log("error while playing: ",error)
             }
-        }
+            return
+        }else{
+            const track = await player.search(query);
 
-        guildNode?.insert(track.tracks[0]) // test this
-        
-        console.log(track.tracks[0].duration)
-        
-        console.log("queue size: ",guildNode?.queue.getSize())  //guildNode.queue = queue ??
-        await interaction.reply(`${track.tracks[0].title} added to the queue!`)
-        
-        // setTimeout(() => {
-        //     guildNode?.stop()
-        // }, 4000);
+            // console.log(track.tracks[0])
+            // queue?.insertTrack(track.tracks[0])  // test this
+            guildNode?.insert(track.tracks[0]) // test this
+            
+            console.log(track.tracks[0].duration)
+            
+            console.log("queue size: ",guildNode?.queue.getSize())  //guildNode.queue = queue ??
+            await interaction.reply(`${track.tracks[0].title} added to the queue!`)
+            
+            // setTimeout(() => {
+            //     guildNode?.stop()
+            // }, 4000);
+    
+        }
 
     } catch (error) {
         console.log("search failed: ",error)
