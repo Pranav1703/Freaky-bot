9418104d03918047597041ed6125af8e544c4889
Pranav1703 <pranavsai2005@gmail.com>
2026-04-20 20:08:25 +0530

    re-implement skip command

--- src/commands/music/skip.ts
+++ src/commands/music/skip.ts
@@ -1,4 +1,5 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { ChatInputCommandInteraction, GuildMember, SlashCommandBuilder, VoiceBasedChannel } from "discord.js";
+import queueManager from "../../services/queue/queueManager.js";
 
 export const data = new SlashCommandBuilder()
                         .setName("skip")
@@ -6,5 +7,34 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(interaction:ChatInputCommandInteraction) {
 
+    const member = interaction.member as GuildMember
+    const channel = member.voice.channel as VoiceBasedChannel
+
+    if (!channel) {
+        interaction.ephemeral = true
+        return interaction.reply({
+            content: "You must be in a voice channel to use this command!",
+        });
+    }
+    await interaction.deferReply();
+    if (
+    interaction.guild!.members.me!.voice.channel &&
+    interaction.guild!.members.me!.voice.channel !== channel
+    ) {
+        return interaction.editReply(
+          'I am already playing in a different voice channel!',
+        );
+    }
+
+    const guildId = interaction.guildId!
     
+    const guildPlayer = queueManager.GetOrAddPlayerHandler(guildId)
+    const queue = guildPlayer.queue
+    const player = guildPlayer.player
+
+    const nextSong = queue.shift()
+    if(!nextSong) return interaction.reply("queue is empty.")
+    interaction.reply("Skipped Current song. Playing next song in the queue")
+    player.play(nextSong)
+
 }
